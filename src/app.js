763ce586628ea70405596018b86c297cd783f313
Artem Terenin <artemterenin@gmail.com>
2025-10-19 18:33:29 +0300

    feat: implement localization initialization in app.js using i18next

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import watcher from './controllers/watcher.js';
 import submitHandler from './controllers/handlers.js';
+import i18nextInstance from './i18n.js';
+
+const init = () => {
+  const elements = document.querySelectorAll('[data-translate]');
+  for (const element of elements) {
+    const key = element.dataset.translate;
+    element.innerText = i18nextInstance.t(key);
+  }
+};
 
 const app = () => {
+  init();
+
   const state = {
     rssForm: {
       state: 'waitingForSubmission',
