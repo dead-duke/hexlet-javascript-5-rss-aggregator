5acb5da1a535a94de9d2a65298d8d5ae281c534f
Artem Terenin <artemterenin@gmail.com>
2025-10-19 18:03:21 +0300

    feat: add en.js and ru.js localization files to src/locales

---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+const en = {
+  title: 'RSS aggregator',
+  description: 'Start reading RSS today! It’s easy, it’s beautiful.',
+  rssInputLabel: 'RSS link',
+  addButton: 'Add',
+  rssUrlExample: 'Example',
+  rssSuccessfullySubmitted: 'RSS successfully added',
+  error: {
+    rssFieldRequired: 'This field is required',
+    rssUrlMustBeValid: 'The URL must be a valid URL',
+    rssAlreadyAdded: 'RSS already exists',
+  },
+};
+
+export default en;
---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+const ru = {
+  title: 'RSS агрегатор',
+  description: 'Начните читать RSS сегодня! Это легко, это красиво.',
+  rssInputLabel: 'Ссылка RSS',
+  addButton: 'Добавить',
+  rssUrlExample: 'Пример',
+  rssSuccessfullySubmitted: 'RSS успешно загружен',
+  error: {
+    rssFieldRequired: 'Это поле обязательно для заполнения',
+    rssUrlMustBeValid: 'Ссылка должна быть валидным URL',
+    rssAlreadyAdded: 'RSS уже существует',
+  },
+};
+
+export default ru;
