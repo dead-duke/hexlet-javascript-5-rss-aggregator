6207bb6acc7e5dfb813836fc867c4cdb616dcd60
Artem Terenin <artemterenin@gmail.com>
2025-10-28 14:12:18 +0300

    refactor: update function names and improve code structure in render.js

--- src/views/render.js
+++ src/views/render.js
@@ -1,23 +1,43 @@
 import i18nextInstance from '../i18n.js';
 
-const renderValidationError = (form, formState) => {
-  const input = form.querySelector('input#url-input');
+const renderFormControls = (input, submitButton, formState) => {
+  input.classList.remove('is-invalid');
+  input.removeAttribute('readonly');
+  submitButton.disabled = false;
 
-  if (formState === 'invalid') {
-    input.classList.add('is-invalid');
-  } else {
-    input.classList.remove('is-invalid');
-    input.value = '';
-    input.focus();
+  switch (formState) {
+    case 'invalid':
+      input.classList.add('is-invalid');
+      break;
+
+    case 'validatedAndSubmitted':
+      input.setAttribute('readonly', true);
+      submitButton.disabled = true;
+      break;
+
+    default:
+      input.value = '';
+      input.focus();
+      break;
   }
 };
 
 const renderFeedbackMessage = (feedback, value) => {
-  const isSuccess = value === 'success';
-
-  feedback.classList.toggle('text-success', isSuccess);
-  feedback.classList.toggle('text-danger', !isSuccess);
-  feedback.textContent = isSuccess ? i18nextInstance.t('rssSuccessfullySubmitted') : value;
+  switch (value) {
+    case null:
+      feedback.textContent = '';
+      break;
+    case 'noErrors':
+      feedback.textContent = i18nextInstance.t('rssSuccessfullySubmitted');
+      feedback.classList.add('text-success');
+      feedback.classList.remove('text-danger');
+      break;
+    default:
+      feedback.textContent = i18nextInstance.t(`error.${value}`);
+      feedback.classList.add('text-danger');
+      feedback.classList.remove('text-success');
+      break;
+  }
 };
 
-export { renderValidationError, renderFeedbackMessage };
+export { renderFormControls, renderFeedbackMessage };
