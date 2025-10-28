dbe2f930eeea5428778fd854008e4d07fabbb960
Artem Terenin <artemterenin@gmail.com>
2025-10-28 14:38:04 +0300

    feat: add network requests using Axios and implement error handling in handlers.js

--- src/controllers/handlers.js
+++ src/controllers/handlers.js
@@ -1,20 +1,51 @@
+import axios from 'axios';
+import { ValidationError } from 'yup';
 import urlSchema from '../models/validator.js';
+import xmlParser from '../models/xmlParser.js';
+import ParsingError from '../errors/ParsingError.js';
+
+const getErrorType = (error) => {
+  if (error instanceof ValidationError) {
+    return error.errors[0];
+  }
+  if (axios.isAxiosError(error)) {
+    return 'networkError';
+  }
+  if (error instanceof ParsingError) {
+    return 'parsingError';
+  }
+  return 'unknownError';
+};
+
+const addProxy = (url) => {
+  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app');
+  proxyUrl.searchParams.set('url', url);
+  return proxyUrl.toString();
+};
 
 const submitHandler = (state) => (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const url = formData.get('url').trim();
 
-  urlSchema(state.feeds)
+  urlSchema(state.channels.map(({ url }) => url))
     .validate(url)
     .then(() => {
-      state.feeds.push(url);
-      state.rssForm.feedbackMessage = 'success';
+      state.rssForm.error = null;
+      state.rssForm.state = 'validatedAndSubmitted';
+      return axios.get(addProxy(url));
+    })
+    .then((response) => {
+      const xml = response.data.contents;
+      const data = xmlParser(xml);
+
+      state.channels.unshift({ url, ...data });
+      state.rssForm.error = 'noErrors';
       state.rssForm.state = 'waitingForSubmission';
     })
     .catch((error) => {
-      const errorMessage = error.errors[0];
-      state.rssForm.feedbackMessage = errorMessage;
+      const errorType = getErrorType(error);
+      state.rssForm.error = errorType;
       state.rssForm.state = 'invalid';
     });
 };
