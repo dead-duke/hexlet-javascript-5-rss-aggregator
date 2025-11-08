f283274c694801537ee1c48aa5358bd6ff8cb362
Artem Terenin <artemterenin@gmail.com>
2025-11-08 21:29:45 +0300

    feat: add modal and post handling functions in handlers.js for improved UI interaction

--- src/controllers/handlers.js
+++ src/controllers/handlers.js
@@ -1,6 +1,7 @@
 import urlSchema from '../models/validator.js';
 import classifyError from '../utils/errorClassifier.js';
 import { fetchRssChannel } from '../services/rssService.js';
+import findPostById from '../utils/postFinder.js';
 
 const submitHandler = (state) => (event) => {
   event.preventDefault();
@@ -26,4 +27,46 @@ const submitHandler = (state) => (event) => {
     });
 };
 
-export default submitHandler;
+const openModalHandler = (state) => (event) => {
+  const button = event.target.closest('.btn');
+  if (button) {
+    const postContainer = button.closest('.list-group-item');
+    const postId = postContainer.dataset.postId;
+    const post = findPostById(postId, state.channels);
+
+    state.ui.modal = {
+      isOpen: true,
+      postId: postId,
+      title: post.title,
+      content: post.description,
+      link: post.link,
+    };
+  }
+};
+
+const closeModalHandler = (state) => () => {
+  state.ui.modal = {
+    isOpen: false,
+    postId: null,
+    title: null,
+    content: null,
+    link: null,
+  };
+};
+
+const openPostHandler = (state) => (event) => {
+  const isLink = event.target.closest('a');
+  const isButton = event.target.closest('.btn') && event.type === 'click';
+
+  if (isLink || isButton) {
+    const postContainer = event.target.closest('.list-group-item');
+    const postId = postContainer.dataset.postId;
+
+    if (!state.ui.viewedPosts.has(postId)) {
+      state.ui.lastMarkedPostId = postId;
+      state.ui.viewedPosts.add(postId);
+    }
+  }
+};
+
+export { submitHandler, openModalHandler, closeModalHandler, openPostHandler };
