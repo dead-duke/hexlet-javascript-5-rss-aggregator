36bba8d9e0c1c333a4568b8f246f464e893e3f1c
Artem Terenin <artemterenin@gmail.com>
2025-11-09 17:35:12 +0300

    feat: update eslint.config.js for improved linting rules and configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,25 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
+    plugins: {
+      js,
+      '@stylistic': stylistic,
+    },
     extends: ['js/recommended'],
     languageOptions: {
       globals: { ...globals.browser },
     },
     rules: {
-      semi: ['error', 'never'],
-      'arrow-parens': ['error', 'always'],
-      'brace-style': ['error', '1tbs'],
+      'semi': ['error', 'never'],
       'comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/quote-props': ['error', 'consistent-as-needed'],
     },
   },
 ])
