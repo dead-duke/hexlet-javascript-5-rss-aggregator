1604a083790496e362758e03fc3a5693b41d133a
Artem Terenin <artemterenin@gmail.com>
2025-11-09 18:51:45 +0300

    fix: update eslint.config.js to address manual formatting for tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,23 @@
-import js from '@eslint/js'
 import globals from 'globals'
+import js from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
-import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: {
-      js,
       '@stylistic': stylistic,
     },
-    extends: ['js/recommended'],
     languageOptions: {
       globals: { ...globals.browser },
     },
     rules: {
       'semi': ['error', 'never'],
       'comma-dangle': ['error', 'always-multiline'],
-      '@stylistic/arrow-parens': ['error', 'as-needed'],
-      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/brace-style': ['error', 'stroustrup'],
       '@stylistic/quote-props': ['error', 'consistent-as-needed'],
     },
   },
-])
+]
