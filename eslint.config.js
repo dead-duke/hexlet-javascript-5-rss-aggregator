feec216f4b2dfbe6dac63c8fdba9b2a8816cf449
Artem Terenin <artemterenin@gmail.com>
2025-12-31 23:58:10 +0300

    chore: update ESLint config and add packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,36 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import { includeIgnoreFile } from '@eslint/compat'
+import stylistic from '@stylistic/eslint-plugin'
+import { fileURLToPath } from 'node:url'
+
+const gitIgnorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 export default defineConfig([
+  includeIgnoreFile(gitIgnorePath),
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      'coverage/',
+      '*.log',
+    ],
+  },
+  stylistic.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: {
         ...globals.browser,
-        bootstrap: 'readonly',
+        process: 'readonly',
       },
     },
   },
-]);
+])
